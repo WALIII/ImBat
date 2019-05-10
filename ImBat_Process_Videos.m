bea82f0bb6ffb3162dc9e068aab3800ddd9c63c5
WALIII <bliberti@bu.edu>
2019-05-09 21:44:31 -0700

    update image processing

--- ImBat_Process_Videos.m
+++ ImBat_Process_Videos.m
@@ -1,19 +1,47 @@
-function ImBat_Process_Videos()
+function ImBat_Process_Videos(varargin)
+% ImBat_Process_Videos
+
+% run in extracted folder with videos and .mat files
+
+% Default params
+video = 1;
+audio = 1;
+
+mkdir('processed');
+% User pram:
+  nparams=length(varargin);
+for i=1:2:nparams
+	switch lower(varargin{i})
+		case 'video'
+			video=varargin{i+1};           
+        case 'audio'
+            audio=varargin{i+1};
+%         case 't_smooth'
+% 			b=varargin{i+1};
+%                 files = convn(files, single(reshape([1 1 1] / b, 1, 1, [])), 'same');
+        case 'fname'
+            filename=varargin{i+1};
+    end
+end
+
+
+
 
 % Load in all Tiffs, and concatonate them 
+if video ==1; 
+   
 mov_listing=dir(fullfile(pwd,'*.tiff'));
 mov_listing={mov_listing(:).name};
 
 filenames=mov_listing;
 
 
-disp('Parsing Audio and Video files');
+disp('Parsing Video files');
 
 [nblanks formatstring]=fb_progressbar(100);
 fprintf(1,['Progress:  ' blanks(nblanks)]);
 
 for i=1:length(mov_listing)
-
     [path,file,ext]=fileparts(filenames{i});
 if i ==1;
     Yf = read_file(filenames{i});
@@ -31,16 +59,48 @@ end
 disp('remove artifacts');
 Yf =  ImBat_denoise(Yf);
 
-
 % Save and remove video from ram
+FS_tiff(Yf,'fname','processed/RAW_Video');
+clear Yf;
+end
 
+if audio == 1;
 
-% Motion correction
+% Get the audio data:
+% Load in all Tiffs, and concatonate them 
+mov_listing=dir(fullfile(pwd,'*.mat'));
+filenames={mov_listing(:).name};
 
+disp('Parsing Audio files');
 
-% ROI extraction
+[nblanks formatstring]=fb_progressbar(100);
+fprintf(1,['Progress:  ' blanks(nblanks)]);
 
+for i=1:length(mov_listing)
 
+    [path,file,ext]=fileparts(filenames{i});
+if i ==1;
+    D = load(filenames{i}, 'audio', 'video');
+else
+    D_temp = load(filenames{i}, 'audio', 'video');
+    
+    % Analog Stuff
+    D.audio.nrFrames = D.audio.nrFrames+D_temp.audio.nrFrames;
+    D.audio.data = cat(1,D.audio.data,D_temp.audio.data);
+    D.audio.TotalDurration = D.audio.TotalDurration+D_temp.audio.TotalDurration;
+    D.audio.times = cat(1,D.audio.times,D_temp.audio.times);
+    % video stuff
+    D.video.times = cat(1,D.video.times,D_temp.video.times);
+    D.video.nrFramesTotal = D.video.nrFramesTotal+D_temp.video.nrFramesTotal;
+    
+    clear D_temp 
+end
+end
 
+clear audio
+clear video
+audio = D.audio;
+video = D.video;
 
-
+save('processed/AV_data','audio','video','-v7.3');
+end
