f813580d021b539f20022d52468ff02f62e533a4
WALIII <bliberti@bu.edu>
2019-05-15 16:09:10 -0700

    edit saved events

--- ImBat_FindEvents.m
+++ ImBat_FindEvents.m
@@ -1,9 +1,9 @@
 function ImBat_FindEvents(neuron, out)
 
 
-% sort data:
-[out.video_times_true sorB ] =  sort(out.video_times);
-
+% sort data to correct for weird timstamps
+[~, out.video_times_true] =  sort(out.video_times);
+%Cx = neuron.S(:,out.video_times);
 
 % Movement events:
 figure();
@@ -11,8 +11,8 @@ D = diff(nanmean(out.Location2(:,1:3),2));
 plot(out.Location_time(1:end-1),(abs(D)))
 
 
-Cx = zscore(smooth(mean((full(neuron.S(1:50,1:end-1))),1),200));
-[CxA CxB] = findpeaks(Cx,out.video_times(1:end-1),'MinPeakProminence',4);
+Cx = zscore(smooth(mean(full(neuron.S(1:50,out.video_times(1:end-1))),1),200));
+[CxA CxB] = findpeaks(Cx,out.video_times_true(1:end-1),'MinPeakProminence',4);
 
 % Neural events:
 figure();
