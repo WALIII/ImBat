70619dec5c5231a8a0651f42cc2cddc795ea5ce8
WALIII <bliberti@bu.edu>
2019-05-17 10:05:02 -0700

    do not remove y matrix...

--- CNMFe_align.m
+++ CNMFe_align.m
@@ -184,7 +184,7 @@ disp('Smoothing corrected data...');
 [Y] = ImBat_Filter(Y);
 disp('Saving corrected data...');
 save('processed/Motion_corrected_Data.mat','all_shifts','Y','Ysiz','-v7.3');
-clear Y Ysiz
+%clear Y Ysiz
 % saving Downsampled video
 
  Y = imresize(Y,0.5);
