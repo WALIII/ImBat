4490d478d3fbb2181bb38141997a0633b3f4e446
WALIII <bliberti@bu.edu>
2019-05-21 18:10:58 -0700

    add dff

---
+++ ImBat_Dff.m
@@ -0,0 +1,26 @@
+function [Ymax, Y] = ImBat_Dff(Y);
+% ImBat_Dff
+
+% Make df/f image
+
+% Filter movie
+Y = (convn(Y, single(reshape([1 1 1] /10, 1, 1, [])), 'same'));
+
+% Take median of movie
+Y_med = median(Y,3);
+
+% Subtract median
+Y = Y-Y_med;
+
+% take max
+Ymax = max(Y,[],3);
+figure();
+colormap(gray);
+imagesc(Ymax);
+
+
+
+
+
+
+
--- ImBat_denoise.m
+++ ImBat_denoise.m
@@ -1,4 +1,4 @@
-function FiltA =  ImBat_denoise(video);% % Video recon
+function video =  ImBat_denoise(video);% % Video recon
 % ImBat_denoise.m
 
 % Remove wireless artifacts from CaIm video data
@@ -8,14 +8,14 @@ function FiltA =  ImBat_denoise(video);% % Video recon
 
 display_mov = 0;
 % Additional artifact rejection:
-FiltA = double(video);
+video = single(video);
 
 % remove initial baseline trend:
-baseline = (smooth(squeeze(mean(mean(FiltA(1:20,:,:),1),2)),50));
+baseline = (smooth(squeeze(mean(mean(video(1:20,:,:),1),2)),50));
 baseline2 = baseline-mean(baseline);
 
-for ii = 1: size(FiltA,3);
-    FiltA(:,:,ii) =  FiltA(:,:,ii)-baseline2(ii);
+for ii = 1: size(video,3);
+    video(:,:,ii) =  video(:,:,ii)-baseline2(ii);
 end
 
 figure();
@@ -23,13 +23,14 @@ for i = 1:10; % 100 itterations...
 thresh = 3;
 % remove large offsets
 
-siga = squeeze(mean(mean(FiltA(:,1:20,:),1),2));
-sigb = squeeze(mean(mean(FiltA(1:20,:,:),1),2));
+siga = squeeze(mean(mean(video(:,1:20,:),1),2));
+sigb = squeeze(mean(mean(video(1:20,:,:),1),2));
 sig = (siga+sigb)/2;
 % sig_base = smooth(sig,50);
 % sig_base = sig_base-mean(sig_base);
 sig = zscore(sig);
 sig = detrend(sig);
+sig = abs(sig);
 [k1 k2] = find(sig>thresh);
 hold on;
 plot(sig);
@@ -42,14 +43,14 @@ end
 for ii = 1:size(k1,1)
     if k1(ii)<5
 
-      FiltA(:,:,k1(ii)) = (median(FiltA(:,:,k1(ii)+2:k1(ii)+5),3));
+      video(:,:,k1(ii)) = (median(video(:,:,k1(ii)+2:k1(ii)+5),3));
 
     elseif k1(ii)>size(k1,1)-5
-           FiltA(:,:,k1(ii)) = (median(FiltA(:,:,k1(ii)-4:k1(ii)-2),3));
+           video(:,:,k1(ii)) = (median(video(:,:,k1(ii)-4:k1(ii)-2),3));
 
     else
 
-    FiltA(:,:,k1(ii)) = (median(FiltA(:,:,k1(ii)-4:k1(ii)-2),3)+median(FiltA(:,:,k1(ii)+2:k1(ii)+5),3))/2;
+    video(:,:,k1(ii)) = (median(video(:,:,k1(ii)-4:k1(ii)-2),3)+median(video(:,:,k1(ii)+2:k1(ii)+5),3))/2;
 
     end
 end
@@ -57,32 +58,32 @@ end
 
 % detrend data:
 
-baseline = (smooth(squeeze(mean(mean(FiltA(1:20,:,:),1),2)),50));
+baseline = (smooth(squeeze(mean(mean(video(1:20,:,:),1),2)),50));
 baseline2 = baseline-mean(baseline);
 
-for ii = 1: size(FiltA,3);
-    FiltA(:,:,ii) =  FiltA(:,:,ii)-baseline2(ii);
+for ii = 1: size(video,3);
+    video(:,:,ii) =  video(:,:,ii)-baseline2(ii);
 end
-figure(); plot(smooth(squeeze(mean(mean(FiltA(1:20,:,:),1),2)),50));
+figure(); plot(smooth(squeeze(mean(mean(video(1:20,:,:),1),2)),50));
 
 % % smooth video:
 disp('smoothing video');
 b = 3; %(smoothing )
-% FiltB = mat2gray(FiltA-FBM);
-FiltA = (convn(FiltA, single(reshape([1 1 1] / b, 1, 1, [])), 'same'));
+% FiltB = mat2gray(video-FBM);
+video = (convn(video, single(reshape([1 1 1] / b, 1, 1, [])), 'same'));
 mG = mean(video,3);
-mF = mean(FiltA,3);
+mF = mean(video,3);
 
 
 if display_mov ==1;
 figure();
-for i = 1:3:size(FiltA,3)
+for i = 1:3:size(video,3)
     colormap(gray)
  subplot(1,2,1)
 imagesc(double(video(:,:,i))-mG,[-20 60]);
 
     subplot(1,2,2)
-imagesc(FiltA(:,:,i)-mF,[-20 60]);
+imagesc(video(:,:,i)-mF,[-20 60]);
 title('raw video');
 
 %     subplot(1,3,3)
