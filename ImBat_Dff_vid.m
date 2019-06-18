8f1e6ad2f8bddd007526be6d5240831cb5037bc3
Tobias <tschmid@berkeley.edu>
2019-06-18 11:53:44 -0700

    added save function to video and

---
+++ ImBat_Dff_vid.m
@@ -0,0 +1,50 @@
+function [Ymax, df] = ImBat_Dff_vid(Y,fileName);
+% ImBat_Dff
+vidLimits = [55 80];
+% Make df/f image
+
+% Filter movie
+Y = (convn(Y, single(reshape([1 1 1] /10, 1, 1, [])), 'same'));
+
+% Take median or mean of movie
+Y_med = median(Y,3);
+Y_mean = mean(Y,3);
+
+% Subtract median or mean for df
+YmedSub = Y-Y_med;
+df = Y - Y_mean; 
+
+% take max
+Ymax = max(df,[],3);
+Ymax = imresize(Ymax,1);
+Ym = Ymax +100;
+fig = figure();
+colormap(gray);
+imagesc(Ym);
+axis off;
+saveas(fig,[fileName '.tif']);
+
+df = mat2gray(df)*100;
+mov = figure();
+v = VideoWriter([fileName '_' vidLimits '.avi']);
+open(v);    
+
+
+for i=1:4:length(df)
+    
+    imagesc(mean(df(:,:,i:i+3),3),vidLimits);
+    axis off;
+        colormap(gray);
+        frame = getframe(mov);
+   writeVideo(v,frame);
+   %disp(i);
+    pause(0.001);
+ 
+end
+close(v);
+
+
+
+
+
+
