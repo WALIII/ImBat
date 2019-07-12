6b1223838fe81796617c74b668c201375f1fe3ca
WALIII <bliberti@bu.edu>
2019-07-11 18:38:42 -0700

    being hippocrititical about hard coding directories

--- ImBat_MultiDayAnalysis.m
+++ ImBat_MultiDayAnalysis.m
@@ -15,7 +15,7 @@ ST3 = 1; % Across day analysis
 
 % local Directory:
 %LD =  'F:\Bat_search';
-LD = '/Users/ARGO/Documents/DATA/Bat_Data';
+LD = '/Users/ARGO/Documents/DATA/Bat_Data_new';
 mkdir(LD);
 
 % Cell array containing strings of the days you want to look at
