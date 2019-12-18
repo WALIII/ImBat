6de2b4d92ed2964e90720044a9154b2fe86dba9c
WALIII <bliberti@bu.edu>
2019-12-18 12:54:16 -0800

    update to rigid motion correction

--- ImBat_Filter.m
+++ ImBat_Filter.m
@@ -1,6 +1,6 @@
 function  [out_mov] = ImBat_Filter(mov);
 % 
-b = 10; %(smoothing )
+b = 9; %(smoothing )
 % FiltB = mat2gray(FiltA-FBM);
 % Scale data:
 
@@ -9,7 +9,7 @@ disp('scaling data');
 mov = mat2gray(mov)*256;
 
 disp(['smoothing data by a factor of ', num2str(b)]);
-out_mov = medfilt3(A,[1 1 b]);
+out_mov = medfilt3(mov,[1 1 b]);
 
 
 %out_mov = (convn(mov, single(reshape([1 1 1] / b, 1, 1, [])), 'same'));
--- cnmfe/ImBat_START.m
+++ cnmfe/ImBat_START.m
@@ -9,8 +9,11 @@ function ImBat_START(varargin)
 
 % Core Functions:
 
-% ImBat_processVideos: contains 'ImBat_denoise.m'
+% ImBat_processVideos:
+%           contains 'ImBat_denoise.m' For de-noising video ( artifact rejection)
+%                    'CNMFe_align.m' For motion correction ( rigid/non-rigid)
 
+% CNMFe_extract: CNMF extraction
 
 
 
@@ -24,13 +27,13 @@ reExtract =0;
 % Manual inputs
 vin=varargin;
 for i=1:length(vin)
-  if isequal(vin{i},'ROI') % manually inputing a sort order
-    ROI_flag=vin{i+1};
-  elseif isequal(vin{i},'Place')
-      Analysis_flag = vin{i+1};
-  elseif isequal(vin{i},'reExtract');
-      reExtract=vin{i+1};
-end
+    if isequal(vin{i},'ROI') % manually inputing a sort order
+        ROI_flag=vin{i+1};
+    elseif isequal(vin{i},'Place')
+        Analysis_flag = vin{i+1};
+    elseif isequal(vin{i},'reExtract');
+        reExtract=vin{i+1};
+    end
 end
 
 
@@ -44,88 +47,89 @@ dirFlags = [files.isdir];
 subFolders = files(dirFlags);
 % Print folder names to command window.
 for k = 1 : length(subFolders)
-	fprintf('Sub folder #%d = %s\n', k, subFolders(k).name);
+    fprintf('Sub folder #%d = %s\n', k, subFolders(k).name);
 end
 
 
 %% Extraction, Motion Correction, ROI extraction
 
+
 % For each folder ( extraction, and motion correction step)
 for i = 1:length(subFolders);
-disp(['entering folder', char(subFolders(i).name)])
+    disp(['entering folder', char(subFolders(i).name)])
 
-cd([subFolders(i).folder,'/',subFolders(i).name]);
+    cd([subFolders(i).folder,'/',subFolders(i).name]);
 
-% index every subfolder...
-flight_files = dir(pwd);
-flight_files(ismember( {flight_files.name}, {'.', '..','Processed'})) = [];  %remove . and .. and Processed
-flight_dirFlags = [flight_files.isdir];% Get a logical vector that tells which is a directory.
-flight_subFolders = flight_files(flight_dirFlags);% Extract only those that are directories.
+    % index every subfolder...
+    flight_files = dir(pwd);
+    flight_files(ismember( {flight_files.name}, {'.', '..','Processed'})) = [];  %remove . and .. and Processed
+    flight_dirFlags = [flight_files.isdir];% Get a logical vector that tells which is a directory.
+    flight_subFolders = flight_files(flight_dirFlags);% Extract only those that are directories.
 
 
-for ii = 1:length(flight_subFolders);
-cd([subFolders(i).folder,'/',subFolders(i).name]);
+    for ii = 1:length(flight_subFolders);
+        cd([subFolders(i).folder,'/',subFolders(i).name]);
 
-% Check if folder exists
-if exist([flight_subFolders(ii).folder,'/',flight_subFolders(ii).name,'/','processed','/','Motion_corrected_Data_DS.mat'])>0;
-    disp('Folder already extracted..');
-    if reExtract ==1
-        disp('Re-Extracting...');
-    else
-        disp('Moving to the next folder...');
-        extract = 0 ;
+        % Check if folder exists
+        if exist([flight_subFolders(ii).folder,'/',flight_subFolders(ii).name,'/','processed','/','Motion_corrected_Data_DS.mat'])>0;
+            disp('Folder already extracted..');
+            if reExtract ==1
+                disp('Re-Extracting...');
+            else
+                disp('Moving to the next folder...');
+                extract = 0 ;
+            end
+        end
+
+
+        % load tracking data
+        track_fname = flight_subFolders(ii).name;
+        track_fname = extractBefore( track_fname,'_extraction');
+        track_fname = [track_fname,'_track.mat'];
+        load(track_fname);
+
+
+        %%====[ Motion Correction ]======%%
+
+        cd([flight_subFolders(ii).folder,'/',flight_subFolders(ii).name])% index into the flight_subfolder
+        if extract ==1;
+            % Run processing script
+            mkdir('processed');
+            ImBat_processVideos('downsample',0.5);
+            disp('processing!!');
+        end
+
+        cd('processed') % move to processed folder...
+
+        % Check if roi extraction folder exists
+        if exist([flight_subFolders(ii).folder,'/',flight_subFolders(ii).name,'/','processed','/','Motio_corrected_Data_DS_neurons'])>0;
+            disp('ROIs already extracted..');
+
+            if reROI_extract ==1
+                disp('Re-Extracting ROIs...');
+            else
+                disp('Moving to the next folder...');
+                ROI_flag = 0 ;
+            end
+        end
+
+
+        %%====[ CNMF-e ROI Extraction ]======%%
+        if ROI_flag ==1;
+            disp('extracting ROIs...')
+            nam = './Motion_corrected_Data_DS.mat'
+            CNMFe_extract(nam);
+        end
+
+
+        %%====[ Aligning Time Stamps ]======%%
+        % to do: add logfile
+        disp('Aligning Timestamps...');
+        load('AV_data.mat');
+        [out] = ImBat_alignTimeStamps(audio,video,AnalogSignals,Markers);
+
+        clear video audio Markers AnalogSignals out % Clear vars from RAM
+        extract =1;
     end
-end
-
-
-    % load tracking data
-    track_fname = flight_subFolders(ii).name;
-    track_fname = extractBefore( track_fname,'_extraction');
-    track_fname = [track_fname,'_track.mat'];
-    load(track_fname);
-
-
-%%====[ Motion Correction ]======%%
-
-cd([flight_subFolders(ii).folder,'/',flight_subFolders(ii).name])% index into the flight_subfolder
-if extract ==1;
-% Run processing script
-mkdir('processed');
-ImBat_processVideos('downsample',0.5);
-disp('processing!!');
-end
-
-cd('processed') % move to processed folder...
-
-% Check if roi extraction folder exists
-if exist([flight_subFolders(ii).folder,'/',flight_subFolders(ii).name,'/','processed','/','Motio_corrected_Data_DS_neurons'])>0;
-    disp('ROIs already extracted..');
-
-    if reROI_extract ==1
-        disp('Re-Extracting ROIs...');
-    else
-        disp('Moving to the next folder...');
-        ROI_flag = 0 ;
-    end
-end
-
-
-%%====[ CNMF-e ROI Extraction ]======%%
-if ROI_flag ==1;
-    disp('extracting ROIs...')
-    nam = './Motion_corrected_Data_DS.mat'
-    CNMFe_extract(nam);
-end
-
-
-%%====[ Aligning Time Stamps ]======%%
-% to do: add logfile
-disp('Aligning Timestamps...');
-load('AV_data.mat');
-[out] = ImBat_alignTimeStamps(audio,video,AnalogSignals,Markers);
-
-clear video audio Markers AnalogSignals out % Clear vars from RAM
-extract =1;
-end
 
 end
