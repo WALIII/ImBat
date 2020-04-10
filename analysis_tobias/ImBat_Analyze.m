46f8824aab735c32cdb4ec0038c4ae68c49f44cf
Tobias Schmid <tschmid@berkeley.edu>
2020-04-09 17:50:57 -0700

    Small change

--- analysis_tobias/ImBat_Analyze.m
+++ analysis_tobias/ImBat_Analyze.m
@@ -31,9 +31,9 @@ plotFlightsFlag = 1;
 flightPathsAllFlag = 1;
 flightPathsFeederFlag = 1;
 %place cells plot flags
-plotPlaceCellsFlag = 1 ;
+plotPlaceCellsFlag = 0 ;
 %snake/schnitz plot flags
-plotSnakesFlag = 1;
+plotSnakesFlag = 0;
 
 % Get all folders in directory
 files = dir(pwd);
--- analysis_tobias/ImBat_plotFlightsVsCells.m
+++ analysis_tobias/ImBat_plotFlightsVsCells.m
@@ -66,8 +66,8 @@ smoothAvgSpiking = zscore(smooth(mean((full(cellData.results.C_raw(1:topROILocal
             plot(alignment.out.video_timesDS,(zscore(smoothdata(cellData.results.C_raw(i,:),'movmedian',3)))+i*2) %may have to tweak the +i*6 at the end
         catch
             diffVidTimes =  length(cellData.results.C_raw(i,:)) - length(alignment.out.video_timesDS)
-            eplot(alignment.out.video_timesDS,(zscore(smoothdata(cellData.results.C_raw(i,1:end-diffVidTimes),'movmedian',3)))+i*2) %may have to tweak the +i*6 at the end
-            %plot(alignment.out.video_timesDS(1:end-1),(zscore(smoothdata(cellData.results.C_raw(i,:),'movmedian',3)))+i*2) %may have to tweak the +i*6 at the end
+            %plot(alignment.out.video_timesDS,(zscore(smoothdata(cellData.results.C_raw(i,1:end-diffVidTimes),'movmedian',3)))+i*2) %may have to tweak the +i*6 at the end
+            plot(alignment.out.video_timesDS(1:end-1),(zscore(smoothdata(cellData.results.C_raw(i,:),'movmedian',3)))+i*2) %may have to tweak the +i*6 at the end
         end
     end 
     sgtitle(['Velocity vs Cell Activity: ' batName ' ' dateSesh ' ' sessionType])
