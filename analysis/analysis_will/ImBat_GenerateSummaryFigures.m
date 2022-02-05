15c9ff8f2feabe8c67845000f51a59882253409a
WALIII <bliberti@bu.edu>
2022-02-04 21:25:03 -0800

    add source data plotting

--- analysis/analysis_will/ImBat_GenerateSummaryFigures.m
+++ analysis/analysis_will/ImBat_GenerateSummaryFigures.m
@@ -72,7 +72,7 @@ if extract_data ==1;
     end
     
 end
-% Now, you can load the data, and run batch proccessing scripts and make figures
+e% Now, you can load the data, and run batch proccessing scripts and make figures
 
 if make_plots ==1
     disp('Perfoming Summary analysis');
